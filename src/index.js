146502eb43db28c10034f7359b7559be1fe318cf
Ben Miller <bmiller@method.com>
2017-11-02 15:19:20 -0500

    basic shader code working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import './css/main.css';
 
-setTimeout(() => {
-  console.log('hello world');
-}, 500);
+import glsl from 'glslify';
+import createShader from 'gl-shader';
+import triangle from 'a-big-triangle';
+
+import vert from './shaders/vert.glsl';
+import frag from './shaders/vert.glsl';
+
+const shader = createShader(gl, glsl(vert), glsl(frag));
+
+function render() {
+  shader.bind();
+  triangle(gl);
+}
