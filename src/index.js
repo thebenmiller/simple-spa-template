30b23dadf793fb98cf949d8c37948dc13dbd845d
Ben Miller <bmiller@method.com>
2017-11-02 17:04:40 -0500

    basic resizing example

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import './css/main.css';
+require('./css/main.css');
 
-import glsl from 'glslify';
-import createShader from 'gl-shader';
-import triangle from 'a-big-triangle';
+const triangle = require('a-big-triangle');
+const createShader = require('gl-shader');
+const glsl = require('glslify');
+const fit = require('canvas-fit');
 
-import vert from './shaders/vert.glsl';
-import frag from './shaders/vert.glsl';
+const gl = require('webgl-context')({
+  width: window.innerWidth,
+  height: window.innerHeight
+});
 
-const shader = createShader(gl, glsl(vert), glsl(frag));
+const canvas = gl.canvas;
+document.body.appendChild(canvas);
+window.addEventListener('resize', fit(canvas), false);
+
+const vert = require('./shaders/vert.glsl');
+const frag = require('./shaders/frag.glsl');
+
+const shader = createShader(gl, vert, frag);
+shader.bind();
+render();
 
 function render() {
+  const width = gl.drawingBufferWidth;
+  const height = gl.drawingBufferHeight;
+  gl.viewport(0, 0, width, height);
+
   shader.bind();
   triangle(gl);
 }
